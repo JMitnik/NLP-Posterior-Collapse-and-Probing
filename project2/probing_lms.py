a39c795350c61f94db7cd871fbc119d7841d3385
Jonathan <jmitnik@gmail.com>
2020-05-17 14:16:31 +0200

    Python3

--- project2/probing_lms.py
+++ project2/probing_lms.py
@@ -9,9 +9,9 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.4.2
 #   kernelspec:
-#     display_name: xpython
+#     display_name: Python 3
 #     language: python
-#     name: xpython
+#     name: python3
 # ---
 
 # %% [markdown]
